8c2ddcdb25a0f4e71345ca995206ab06aa6fe807
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-08-29 16:46:24 -0600

    Initial Commit

---
+++ HierarchicalNavigationPracticeMeta/HierarchicalNavigationPracticeMetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HierarchicalNavigationPracticeMetaApp.swift
+//  HierarchicalNavigationPracticeMeta
+//
+//  Created by Mitch Andrade on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct HierarchicalNavigationPracticeMetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
